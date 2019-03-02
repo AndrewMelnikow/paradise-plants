f7b7bb0cac858e7122b2c6d8099430a747fb721a
Andrew <39967997+AndrewMelnikow@users.noreply.github.com>
2019-03-02 12:52:20 +0200

    Update component_connectdb.php

--- comp/component_connectdb.php
+++ comp/component_connectdb.php
@@ -1,9 +1,9 @@
 <?php
 try {
     $pdo = new PDO(
-        'mysql:host=localhost;dbname=eshop', //'mysql:host=localhost;dbname=id8169918_eshop',
-        'root', //'id8169918_admin', 
-        '', //'kengurukenguru1',
+        'mysql:host=localhost;dbname=eshop', 
+        'root',
+        '',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
 } catch (PDOException $e) {
     echo '<div class="alert alert-danger">';
